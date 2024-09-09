7356bc9d63d6be69628fdbdc6e1300cc793d42a8
l66612472@gmail.com <l66612472@gmail.com>
2024-09-09 21:08:25 +0900

    ♻️  Refactor: 로그인 성공 시 응답만 반환 및 findUserById 경로 변경

--- server/src/modules/auth/auth.repository.ts
+++ server/src/modules/auth/auth.repository.ts
@@ -8,6 +8,7 @@
  * 2024.09.07    이승철      Created
  * 2024.09.07    이승철      Modified    db 유저 조회, 생성, rf토큰 업데이트 로직 구현
  * 2024.09.08    이승철      Modified    닉네임 중복확인 로직
+ * 2024.09.09    이승철      Modified    findUserById 메서드 user.repository로 경로 변경
  */
 
 import { UserDto } from '@_auth/dto/user.dto';
@@ -33,11 +34,6 @@ export class AuthRepository {
   // 사용자 닉네임으로 사용자 조회 (닉네임 중복 확인용)
   async findUserByNickname(nickName: string): Promise<User | null> {
     return this.userRepository.findOne({ where: { nickName } });
-  }
-
-  // 사용자 ID로 사용자 조회
-  async findUserById(id: number): Promise<User | null> {
-    return this.userRepository.findOne({ where: { id, deletedAt: null } });
   }
 
   // 사용자 생성
