87aa847dee662acb72d392474fd14cce6c6b1538
somgyul <namhee9096@gmail.com>
2024-09-09 22:42:45 +0900

    :sparkles: feat: 헤더 컴포넌트router 연결

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,18 +7,21 @@
  * Date          Author      Status      Description
  * 2024.09.05    김민지      Created
  * 2024.09.07    김민지      Modified       초기화
+ * 2024.09.09    황솜귤      Created     헤더 라우터 설정
  */
 
-import { Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/header-component';
 
 function App() {
   return (
-    <Routes>
-      {/* <Route path='/' element={<Root />} /> */}
-      {/* <Route path='/my' element={<My />} /> */}
-    </Routes>
-  );
+    <Router>
+     <Header /> {/* 헤더를 모든 페이지에 공통으로 표시 */}
+      <Routes>
+     </Routes>
+    </Router>
+);
 }
 
 export default App;
