26f7188b8e9917f442dcdd2d9c7aa538aad32b51
[AI11] 김민지 <kkimming__@naver.com>
2024-09-11 03:21:06 +0900

    💡 Comment: 파일 주석 추가

--- client/src/pages/LifeGraph/GraphList.tsx
+++ client/src/pages/LifeGraph/GraphList.tsx
@@ -1,7 +1,20 @@
+/*
+ * File Name    : GraphList.tsx
+ * Description  : 그래프 ID에 해당하는 그래프 컴포넌트
+ * Author       : 김민지
+ *
+ * History
+ * Date          Author      Status      Description
+ * 2024.09.10    김민지      Created
+ * 2024.09.11    김민지      Modified    더미데이터로 그래프 생성 및 디자인 테스트
+ * 2024.09.13    김재영      Done        관련 채팅방 검색 및 참여, 커뮤니티 피드 제공 기능 추가 및 완료
+ */
+
 import { Graph } from '../../components/Graph/Graph';
+import { lifeDataArray } from '../../types/graph_type';
 
 // res.data
-const lifeData = [
+const lifeData: lifeDataArray = [
   { age: 17, score: 4, title: '일본 유학' },
   { age: 20, score: 3, title: '대학 입학', desc: '엘리스 대학교 AI학과 입학' },
   { age: 21, score: 4, title: '유럽 여행' },
