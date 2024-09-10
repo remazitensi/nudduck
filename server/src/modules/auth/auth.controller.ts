f40330371a0a3f13dc74f9ab2dcff880bc57c151
l66612472@gmail.com <l66612472@gmail.com>
2024-09-11 06:46:03 +0900

    :sparkles: Feat: 엑세스 토큰 재발급 재추가

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -10,17 +10,19 @@
  * 2024.09.08    이승철      Modified    예외처리
  * 2024.09.09    이승철      Modified    로그인 성공 시 응답만 전달, 클라이언트에서 redirect
  * 2024.09.09    이승철      Modified    configService 삭제
- * 2024.09.10    이승철      Modified    refreshToken 재발급 api 삭제, 만료시 클라이언트에서 재로그인 유도
+ * 2024.09.10    이승철      Modified    accessToken 재발급 api 삭제, 만료시 클라이언트에서 재로그인 유도
  * 2024.09.10    이승철      Modified    userDto 구조분해할당
+ * 2024.09.10    이승철      Modified    accessToken 재발급 api 재추가
  */
 
 import { AuthService } from '@_auth/auth.service';
+import { RefreshTokenDto } from '@_auth/dto/refresh-token.dto';
 import { UserDto } from '@_auth/dto/user.dto';
-import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { OAuthUser } from '@_auth/interface/oauth-user.interface';
+import { BadRequestException, Body, Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
-import { OAuthUser } from './interface/oauth-user.interface';
 
 @ApiTags('Authentication')
 @Controller('auth')
@@ -69,5 +71,18 @@ export class AuthController {
 
     await this.authService.socialLogin(userDto, res);
     res.status(200).send('Login successful'); // 성공 응답을 주고 클라이언트에서 redirect
+  }
+
+  @ApiOperation({ summary: '엑세스 토큰 재발급' })
+  @Post('access-token')
+  async accessToken(@Body() refreshTokenDto: RefreshTokenDto, @Res() res: Response) {
+    if (!refreshTokenDto.refreshToken) {
+      throw new BadRequestException('리프레시 토큰이 제공되지 않았습니다.');
+    }
+
+    await this.authService.regenerateAccessToken(refreshTokenDto.refreshToken, res);
+
+    // 성공 응답만 반환
+    return res.status(200).json({ message: '엑세스 토큰이 재발급되었습니다.' });
   }
 }
--- server/src/modules/auth/auth.service.ts
+++ server/src/modules/auth/auth.service.ts
@@ -11,16 +11,17 @@
  * 2024.09.08    이승철      Modified    회원가입 시, 임의로 닉네임 추가
  * 2024.09.09    이승철      Modified    findUserById 메서드 user.repository로 경로 변경
  * 2024.09.09    이승철      Modified    쿠키에 sameSite:lax(CSRF 방지) 추가
- * 2024.09.10    이승철      Modified    refreshToken 재발급 로직 삭제 및 유효기간 3일로 변경
+ * 2024.09.10    이승철      Modified    accessToken 재발급 로직 삭제 및 유효기간 3일로 변경
  * 2024.09.10    이승철      Modified    회원가입 시, 기본이미지 설정 로직 추가
  * 2024.09.10    이승철      Modified    회원탈퇴 후, 재가입 로직 구현
+ * 2024.09.11    이승철      Modified    accessToken 재발급 로직 재추가
  */
 
 import { AuthRepository } from '@_auth/auth.repository';
 import { UserDto } from '@_auth/dto/user.dto';
 import { FileUploadService } from '@_file-upload/file-upload.service';
 import { User } from '@_user/entity/user.entity';
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Response } from 'express';
@@ -126,6 +127,37 @@ export class AuthService {
       secure: true,
       sameSite: 'lax',
       maxAge: 3 * 24 * 60 * 60 * 1000, // 3일
+    });
+  }
+
+  // 리프레시 토큰 검증 및 AccessToken 재발급
+  async regenerateAccessToken(refreshToken: string, res: Response): Promise<void> {
+    const payload = this.jwtService.verify(refreshToken, {
+      secret: this.configService.get('JWT_REFRESH_SECRET'),
+    });
+
+    const user = await this.authRepository.findUserByProvider(payload.provider, payload.sub);
+    if (!user) {
+      throw new UnauthorizedException('사용자를 찾을 수 없습니다.');
+    }
+
+    if (user.refreshToken !== refreshToken) {
+      throw new ForbiddenException('리프레시 토큰이 일치하지 않습니다.');
+    }
+
+    const accessToken = this.jwtService.sign(
+      { sub: user.id, provider: user.provider, email: user.email },
+      {
+        secret: this.configService.get('JWT_ACCESS_SECRET'),
+        expiresIn: '1h',
+      },
+    );
+
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'lax',
+      maxAge: 60 * 60 * 1000,
     });
   }
 }
