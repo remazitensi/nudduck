45d64db6a8522bc611e0343c1d8f953581201cfe
[AI11] 김민지 <kkimming__@naver.com>
2024-09-12 01:36:06 +0900

    ✨ Feat: baseApi credential:true 추가

--- client/src/apis/base-api.ts
+++ client/src/apis/base-api.ts
@@ -6,11 +6,13 @@
  * History
  * Date          Author      Status      Description
  * 2024.09.10    김민지      Created      환경변수로 base-api 설정
+ * 2024.09.11    김민지      Modified     withCredential : true
  */
 
 import axios from 'axios';
 
 export const api = {
+  auth: '/auth',
   home: '/home',
   myPage: '/my',
   simulation: '/simulation',
@@ -21,6 +23,7 @@ export const api = {
 };
 
 export const baseApi = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
-  timeout: 5000,
+  baseURL: import.meta.env.VITE_BASE_URL, // Vite 환경 변수로 설정된 URL
+  timeout: 5000, // 5초 후 타임아웃
+  withCredentials: true, // 자격 증명 포함
 });
