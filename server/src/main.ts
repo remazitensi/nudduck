aabb5a586a167588a21c37906edd45ebf5d4380a
l66612472@gmail.com <l66612472@gmail.com>
2024-09-12 00:43:19 +0900

    :wrench: Config: globalPrefix로 /api 추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,6 +8,7 @@
  * 2024.09.07    김재영      Created
  * 2024.09.07    이승철      Modified    ValidationPipe 추가 및 cookieParser 설정
  * 2024.09.10    이승철      Modified    트랜잭션 컨텍스트를 초기화
+ * 2024.09.12    이승철      Modified    GlobalPrefix 설정
  */
 
 import { ValidationPipe } from '@nestjs/common';
@@ -20,6 +21,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   initializeTransactionalContext();
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // 아무 decorator도 없는 object를 걸러준다.
