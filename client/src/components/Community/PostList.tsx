1ac257b694891b9d1107f3c4740a85ff73972ddc
[AI11] 김민지 <kkimming__@naver.com>
2024-09-13 05:46:00 +0900

    ✨ Feat: 게시글 목록 동적 추가

--- client/src/components/Community/PostList.tsx
+++ client/src/components/Community/PostList.tsx
@@ -6,27 +6,18 @@
  * History
  * Date          Author      Status      Description
  * 2024.09.11    김민지      Created      getPostList 추가 : 로드시 get 요청
+ * 2024.09.13    김민지      Modified     PostSection 동적 추가
  */
 
-import { useEffect, useState } from 'react';
-import { getPostList } from '../../apis/community-api';
+import { PostSection } from './PostSection';
 
-export const PostList: React.FC = () => {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const data = await getPostList({ category: '', page: 1, sort: 'latest' });
-        setPosts(data); // 받아온 데이터로 state 업데이트
-        console.log(posts);
-      } catch (error) {
-        console.error('Failed to fetch posts:', error.response.data.message); // 에러 처리
-      }
-    };
-
-    fetchPosts();
-  }, []);
-
-  return <div>{/* TODO : post 데이터를 받아가는 게시글 컴포넌트 */}</div>;
+export const PostList: React.FC<any> = ({ posts }) => {
+  return (
+    <div>
+      {/* 각 게시글 데이터를 PostSection에 전달 */}
+      {posts.map((post) => (
+        <PostSection key={post.post_id} data={post} />
+      ))}
+    </div>
+  );
 };
