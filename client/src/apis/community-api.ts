2a11e1e1b5ff97b1b2d677df41406de4c5122736
[AI11] 김민지 <kkimming__@naver.com>
2024-09-13 05:44:45 +0900

    ✨Feat : 게시글 조회, 작성, 수정, 삭제 api 추가

--- client/src/apis/community-api.ts
+++ client/src/apis/community-api.ts
@@ -1,12 +1,12 @@
 /*
- * File Name    : base-api.ts
- * Description  : base-api create
+ * File Name    : community-api.ts
+ * Description  : community-api create
  * Author       : 김민지
  *
  * History
  * Date          Author      Status      Description
  * 2024.09.10    김민지      Created      환경변수로 base-api 설정
- * 2024.09.12    김민지      Modified     type 지정
+ * 2024.09.13    김민지      Modified     게시글 get api 추가
  */
 
 import { postDetail, postList } from '../types/community-type';
@@ -30,7 +30,7 @@ export async function getPostList({ category = '', page = 1, sort = 'latest', se
     console.log(response);
     return response.data; // 성공 시 data 반환
   } catch (error) {
-    throw error; // 실패 시 error를 throw
+    return console.error('Failed to fetch posts:', error.response.data.message); // 실패 시 error message
   }
 }
 
@@ -43,9 +43,68 @@ export async function getPostDetail({ category = '', id = '' }: postDetail): Pro
     console.log(response);
     return response.data;
   } catch (error) {
-    throw error;
+    return console.error('Failed to fetch posts:', error.response.data.message); // 실패 시 error message
   }
 }
+
+const testPost = {
+  post_id: 5,
+  title: '제목입니다울랄라',
+  content: '내용입니다.',
+  user_id: '1004',
+  category: 'study',
+  created_at: '2024-09-12T12:34:56.789Z',
+  updated_at: '2024-09-12T12:34:56.789Z',
+  likes_count: 10,
+  views_count: 100,
+  comments_count: 5,
+};
+
+// 게시글 작성 post 요청
+export async function writePost({ testPost }) {
+  try {
+    const res = await baseApi.post(api.community, { testPost });
+
+    if (res.status === 201) {
+      const post_id = res.data.post_id;
+      window.location.href = `${api.community}/${post_id}`;
+    }
+  } catch (error) {
+    return console.error('Failed to fetch posts:', error.response.data.message); // 실패 시 error message
+  }
+}
+
+// 게시글 수정 put 요청
+export async function editPost({ testPost }) {
+  try {
+    const res = await baseApi.put(`${api.community}/${testPost.post_id}`, {});
+
+    if (res.status === 201) {
+      window.location.href = `${api.community}`;
+    }
+  } catch (error) {
+    return console.error('Failed to fetch posts:', error.response.data.message); // 실패 시 error message
+  }
+}
+
+// 게시글 삭제 delete 요청
+export async function deletePost({ testPost }) {
+  try {
+    const res = await baseApi.delete(`${api.community}/${testPost.post_id}`, {});
+
+    if (res.status === 201) {
+      window.location.href = `${api.community}`;
+    }
+  } catch (error) {
+    return console.error('Failed to fetch posts:', error.response.data.message); // 실패 시 error message
+  }
+}
+
+// TODO : 게시글 좋아요 증가
+
+// TODO : 게시글 좋아요 감소
+
+// TODO : 게시글 조회수 증가
 
 // <-------------- 댓글 ------------------>
 
