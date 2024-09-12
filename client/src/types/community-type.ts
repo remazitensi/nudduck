8467cf3bfd3e09b362a06850125add3b67591e0f
[AI11] 김민지 <kkimming__@naver.com>
2024-09-13 05:49:59 +0900

    🏷️ Types: 게시글 타입 추가

--- client/src/types/community-type.ts
+++ client/src/types/community-type.ts
@@ -1,5 +1,14 @@
 //
 
-export type postList = { category?: string; page: number; sort: string; search: string };
+export type postList = { category?: string; page?: number; sort?: string; search?: string };
 
 export type postDetail = { category?: string; id: string };
+
+export type PostData = {
+  post_id: number;
+  title: string;
+  content: string;
+  category: string;
+  user_id: number;
+  created_at: string;
+};
