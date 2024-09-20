ed603eb3806bdbd2b09eba78bdcd2e8bd11a2444
[AI11] 김우현 <dngus523@naver.com>
2024-09-20 21:40:40 +0900

    👽️ update: api 코드 업데이트

--- client/src/components/MyPage/MyProfile.tsx
+++ client/src/components/MyPage/MyProfile.tsx
@@ -1,9 +1,17 @@
+/**
+ * File Name    : Header.tsx
+ * Description  : pages - 프로필 수정 모달 -
+ * Author       : 김우현
+ *
+ * History
+ * Date          Author      Status      Description
+ * 2024.09.20    김우현      Updated     api 완성()
+ */
 import UserEditModal from '../../pages/UserEditModal';
 
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { fetchUserProfile } from '../../apis/mypage-api';
-
 
 // MyProfile 컴포넌트에 필요한 props 타입 정의
 interface MyProfileProps {
@@ -24,7 +32,7 @@ interface MyProfileProps {
     email: string;
     hashtags: string[];
     created_At: string;
-  } 
+  };
 }
 
 const MyProfile: React.FC<MyProfileProps> = ({
@@ -38,62 +46,55 @@ const MyProfile: React.FC<MyProfileProps> = ({
   handleSaveNickName,
   handleSaveHashTag,
 }) => {
+  // // api 작업
+  const [profile, setProfile] = useState({
+    id: '',
+    imageUrl: '',
+    nickName: '',
+    name: '',
+    email: '',
+    hashtags: [],
+    created_At: '',
+  }); // get으로 받아온 새 정보
 
+  const fetchProfile = async () => {
+    try {
+      const data: typeof profile = await fetchUserProfile(); // profile 타입과 동일하게 설정
+      setProfile(data);
+      console.log('Fetched profile:', data); // API 호출 후 데이터 로그 확인
+    } catch (error) {
+      console.error('Failed to fetch profile:', error);
+    }
+  };
 
-// // api 작업
-const [profile, setProfile] = useState({ 
-  id:'',
-  imageUrl: '',
-  nickName:'',
-  name:'',
-  email:'',
-  hashtags:[],
-  created_At:'',
-}); // get으로 받아온 새 정보
-
-const fetchProfile = async () => {
-  try {
-    const data: typeof profile = await fetchUserProfile(); // profile 타입과 동일하게 설정
-    setProfile(data);
-    console.log('Fetched profile:', data); // API 호출 후 데이터 로그 확인
-  } catch (error) {
-    console.error('Failed to fetch profile:', error);
-  }
-};
-
-
-// api 작업
-useEffect(() => {
+  // api 작업
+  useEffect(() => {
     // const updateProfile = async() => {
     // const data = await fetchProfile();
     // }
-    fetchProfile(); 
-}, [])
+    // fetchProfile();
+  }, []);
 
-
-// profile 상태가 업데이트되었을 때 로그를 출력하기 위한 useEffect
+  // profile 상태가 업데이트되었을 때 로그를 출력하기 위한 useEffect
   useEffect(() => {
     console.log('Updated profile:', profile);
   }, [profile]);
 
-// // api 끝
-
-
-
+  // // api 끝
 
   return (
     <div>
-      <div className='flex flex-col items-center mt-[140px]'>
+      <div className='mt-[140px] flex flex-col items-center'>
         <div className='text-[28px] font-bold'>마이페이지</div>
         <div className='mt-[10px] w-[170px] border-b-2 border-[#8D8B67]'></div>
       </div>
 
-      <div className="flex mt-[80px] w-[1200px] gap-[30px]">
-        <div className="w-[500px] h-[780px] bg-[#fafafa] flex flex-col rounded-[20px] shadow-lg">
-          <div className="pl-[40px] pt-[35px]">
-            <div className="flex gap-[20px]">
-              <div className="text-[25px]">내 프로필</div>
-              <img onClick={handleOpenModal} className="cursor-pointer" src='edit_button.svg' alt='editButton' />
+      <div className='mt-[80px] flex w-[1200px] gap-[30px]'>
+        <div className='flex h-[780px] w-[500px] flex-col rounded-[20px] bg-[#fafafa] shadow-lg'>
+          <div className='pl-[40px] pt-[35px]'>
+            <div className='flex gap-[20px]'>
+              <div className='text-[25px]'>내 프로필</div>
+              <img onClick={handleOpenModal} className='cursor-pointer' src='edit_button.svg' alt='editButton' />
               {open && (
                 <UserEditModal
                   data={profile}
@@ -105,27 +106,31 @@ useEffect(() => {
                 />
               )}
             </div>
-            <div className="mt-[25px]">
-              <p className="text-[16px]">내 정보를 입력하고 커뮤니티에서 더 많은 유저와 소통해요!</p>
+            <div className='mt-[25px]'>
+              <p className='text-[16px]'>내 정보를 입력하고 커뮤니티에서 더 많은 유저와 소통해요!</p>
               <div className='mt-[20px] w-[415px] border-b border-[#585858]'></div>
             </div>
           </div>
-          <div className="flex justify-center">
-            <img className="w-[250px] h-[250px] rounded-[125px] mt-[20px]" src='/userImage.png' alt='userImg' />
+          <div className='flex justify-center'>
+            <img className='mt-[20px] h-[250px] w-[250px] rounded-[125px]' src='/userImage.png' alt='userImg' />
             {/* {profile.imageUrl || '/default_image.png'} */}
           </div>
 
-          <div className="pl-[65px] mt-[60px]">
-            <div className="text-[20px] text-[#909700]">{profile.hashtags}</div>
+          <div className='mt-[60px] pl-[65px]'>
+            {profile.hashtags.map((hashtag, index) => (
+              <div key={index} className='text-[20px] text-[#909700]'>
+                #{hashtag}
+              </div>
+            ))}
 
-            <div className="flex gap-[30px] text-[18px]">
-              <div className="mt-[20px] leading-loose">
+            <div className='flex gap-[30px] text-[18px]'>
+              <div className='mt-[20px] leading-loose'>
                 <div>닉네임</div>
                 <div>이름</div>
                 <div>이메일</div>
                 <div>가입일</div>
               </div>
-              <div className="mt-[20px] leading-loose">
+              <div className='mt-[20px] leading-loose'>
                 <p>{profile.nickName}</p>
                 <p>{profile.name}</p>
                 <p>{profile.email}</p>
--- client/src/pages/QuitModal.tsx
+++ client/src/pages/QuitModal.tsx
@@ -6,56 +6,73 @@
  * History
  * Date          Author      Status      Description
  * 2024.09.10    김우현      Created     탈퇴 모달 생성
+ * 2024.09.20    김우현      Updated     api 완성()
  */
 import React from 'react';
 import { deleteAccount } from '../apis/mypage-api';
 
-
 interface QuitModalProps {
-    onClose: () => void;
+  onClose: () => void;
 }
 
-const QuitModal:React.FC<QuitModalProps> = ({ onClose }) => {
+const QuitModal: React.FC<QuitModalProps> = ({ onClose }) => {
+  const handleClickQuit = async () => {
+    try {
+      console.log('Error:', 'api요청 진행중');
+      await deleteAccount(); // 탈퇴처리 api 호출
 
-    const handleClickQuit = async() => {
-        try {
-            await deleteAccount(); // 탈퇴처리 api 호출
-            alert('정상적으로 탈퇴처리 되었습니다.') // 성공 메시지
-            onClose();
-        } catch (error) {
-            console.log('탈퇴처리 실패:', error);
-            alert('탈퇴 처리중 문제가 발생하였습니다.');
-        }
+      alert('정상적으로 탈퇴처리 되었습니다.'); // 성공 메시지
+      onClose();
+    } catch (error) {
+      console.log('탈퇴처리 실패:', error);
+      alert('탈퇴 처리중 문제가 발생하였습니다.');
     }
+  };
 
-    return (
-        <div className="fixed inset-0 flex justify-center items-center bg-[#585858] bg-opacity-30 cursor-default" onClick={onClose}>
-            <div className="flex flex-col w-[555px] h-[770px] rounded-[20px] bg-white shadow-lg" onClick={(e) =>  e.stopPropagation()}>
-                <div className='flex justify-end'>
-                    <div onClick={onClose} className='pr-[20px] pt-[20px] cursor-pointer'>X</div>
-                </div>
-                {/* &nbsp; 한칸 띄우게 해주는 코드 */}
-                <div className='flex justify-center mt-[80px] text-[34px] text-[#626146] font-bold'>정말&nbsp;<span className="text-[#FFB896]">탈퇴</span>하시나요?</div>
-                <div className='flex justify-center mt-[60px]'>
-                    <div className='flex justify-center'>
-                        <div className='font-bold text-[18px]'>취업 준비생 &nbsp;<span className='font-normal'>님!</span></div>
-                    </div>
-                </div>
-                <div className='flex flex-col items-center mt-[50px] text-[18px]'>
-                    <div>탈퇴하시면 인생 그래프와 AI코치 히스토리가 삭제됩니다.</div>
-                    <div>작성한 게시글은 탈퇴한 사용자의 게시물로 노출됩니다.</div><br/><br/>
-                    <div>별도의 확인 없이 회원 탈퇴가 진행되므로</div>
-                    <div>신중하게 결정해 주세요!</div><br/><br/>
-                    <div>재가입 가능, 게시물 복구...</div><br/><br/>
-                    <div>누떡은 &nbsp;<span className='font-bold'>취업 준비생 &nbsp;</span>님과 더 오래 함께하고 싶어요.</div>
-                </div>
-                <div className='flex justify-center mt-[50px] gap-[10px]'>
-                    <button onClick={handleClickQuit} className='flex items-center justify-center rounded-xl w-[150px] h-[50px] bg-[#FFEABA] text-[24px] font-bold'>탈퇴</button>
-                    <button onClick={onClose} className='flex items-center justify-center rounded-xl w-[150px] h-[50px] bg-[#909700] text-white text-[24px] font-bold'>취소</button>
-                </div>    
-            </div>
+  return (
+    <div className='fixed inset-0 flex cursor-default items-center justify-center bg-[#585858] bg-opacity-30' onClick={onClose}>
+      <div className='flex h-[770px] w-[555px] flex-col rounded-[20px] bg-white shadow-lg' onClick={(e) => e.stopPropagation()}>
+        <div className='flex justify-end'>
+          <div onClick={onClose} className='cursor-pointer pr-[20px] pt-[20px]'>
+            X
+          </div>
         </div>
-    )
+        {/* &nbsp; 한칸 띄우게 해주는 코드 */}
+        <div className='mt-[80px] flex justify-center text-[34px] font-bold text-[#626146]'>
+          정말&nbsp;<span className='text-[#FFB896]'>탈퇴</span>하시나요?
+        </div>
+        <div className='mt-[60px] flex justify-center'>
+          <div className='flex justify-center'>
+            <div className='text-[18px] font-bold'>{/* 취업 준비생 &nbsp;<span className='font-normal'>님!</span> */}</div>
+          </div>
+        </div>
+        <div className='mt-[50px] flex flex-col items-center text-[18px]'>
+          <div>탈퇴하시면 인생 그래프와 AI코치 히스토리가 삭제됩니다.</div>
+          <div>작성한 게시글은 탈퇴한 사용자의 게시물로 노출됩니다.</div>
+          <br />
+          <br />
+          <div>별도의 확인 없이 회원 탈퇴가 진행되므로</div>
+          <div>신중하게 결정해 주세요!</div>
+          <br />
+          <br />
+
+          <br />
+          <br />
+          <div>
+            누떡은 &nbsp;<span className=''>여러분</span>과 더 오래 함께하고 싶어요.
+          </div>
+        </div>
+        <div className='mt-[50px] flex justify-center gap-[10px]'>
+          <button onClick={handleClickQuit} className='flex h-[50px] w-[150px] items-center justify-center rounded-xl bg-[#FFEABA] text-[24px] font-bold'>
+            탈퇴
+          </button>
+          <button onClick={onClose} className='flex h-[50px] w-[150px] items-center justify-center rounded-xl bg-[#909700] text-[24px] font-bold text-white'>
+            취소
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default QuitModal;
