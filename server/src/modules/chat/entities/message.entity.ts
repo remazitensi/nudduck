5466d73b98a6d19c3547342ad67698f29c893eb8
remazitensi <guming0104@gmail.com>
2024-09-20 14:29:57 +0900

    ✨ Feat: 채팅 기능 구현을 위한 사용자 엔티티 추가

--- server/src/modules/chat/entities/message.entity.ts
+++ server/src/modules/chat/entities/message.entity.ts
@@ -5,12 +5,14 @@
  *
  * History
  * Date          Author      Status      Description
- * 2024-09-17    김재영      Created     메시지 엔티티 정의
+ * 2024.09.17    김재영      Created     메시지 엔티티 정의
+ * 2024.09.20    김재영      Modified    메시지 엔티티 수정
  */
 
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, JoinColumn } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 import { ChatRoom } from './chat-room.entity';
+import { User } from './user.entity';
 
 @Entity('ChatMessage')
 export class Message {
@@ -22,17 +24,19 @@ export class Message {
   messageId: number;
 
   @ApiProperty({
-    description: '메시지를 전송한 사용자 ID',
+    description: '메시지를 전송한 사용자',
     example: 101,
   })
-  @Column()
-  UserId: number;
+  @ManyToOne(() => User, { eager: true })
+  @JoinColumn({ name: 'userId' })
+  user: User;
 
   @ApiProperty({
-    description: '메시지가 전송된 채팅방 ID',
+    description: '메시지가 전송된 채팅방',
     example: 1,
   })
-  @ManyToOne(() => ChatRoom, { eager: true })
+  @ManyToOne(() => ChatRoom, (chatRoom) => chatRoom.messages, { eager: true })
+  @JoinColumn({ name: 'chatroomId' })
   chatroom: ChatRoom;
 
   @ApiProperty({
@@ -42,13 +46,17 @@ export class Message {
   @Column('text')
   content: string;
 
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @ApiProperty({
+    description: '메시지가 전송된 시간',
+    example: '2024-09-17T12:34:56Z',
+  })
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   timestamp: Date;
 
-  @ManyToOne(() => Room, (room) => room.messages)
-  @JoinColumn({ name: 'roomId' })
-  room: Room;
-
+  @ApiProperty({
+    description: '메시지 읽음 여부',
+    example: false,
+  })
   @Column({ default: false })
   read: boolean;
 }
