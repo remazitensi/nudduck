78bcdef989d2ffef462a346be16553d9fd83fe05
l66612472@gmail.com <l66612472@gmail.com>
2024-09-21 03:06:33 +0900

    :recycle: Refactor: lifeGraph 모듈 주입

--- server/src/modules/profile/profile.module.ts
+++ server/src/modules/profile/profile.module.ts
@@ -6,15 +6,17 @@
  * History
  * Date          Author      Status      Description
  * 2024.09.17    이승철      Created
+ * 2024.09.21    이승철      Modified    lifeGraph 모듈 주입
  */
 
+import { LifeGraphModule } from '@_modules/life-graph/life-graph.module';
 import { ProfileController } from '@_modules/profile/profile.controller';
 import { ProfileService } from '@_modules/profile/profile.service';
 import { UserModule } from '@_modules/user/user.module';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, LifeGraphModule],
   controllers: [ProfileController],
   providers: [ProfileService],
 })
