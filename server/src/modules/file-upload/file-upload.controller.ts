8addeed727aaadb4c69597a145d552e0b07a933a
l66612472@gmail.com <l66612472@gmail.com>
2024-09-21 05:27:37 +0900

    :pencil: Docs: responseDTO 추가 및 swagger 데코레이터 재정렬

--- server/src/modules/file-upload/file-upload.controller.ts
+++ server/src/modules/file-upload/file-upload.controller.ts
@@ -9,6 +9,7 @@
  * 2024.09.16    이승철      Modified    절대경로 변경
  * 2024.09.19    이승철      Modified    ApiResponse 추가
  * 2024.09.19    이승철      Modified    ApiTags 추가
+ * 2024.09.21    이승철      Modified    응답 dto 추가
  */
 
 import { Jwt } from '@_modules/auth/guards/jwt';
@@ -16,6 +17,7 @@ import { PresignedUrlDto } from '@_modules/file-upload/dto/file-upload.dto';
 import { FileUploadService } from '@_modules/file-upload/file-upload.service';
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { PresignedUrlResponseDto } from './dto/presigned-url-response.dto';
 
 @ApiTags('presigned-url')
 @Controller('presigned-url')
@@ -24,25 +26,11 @@ export class FileUploadController {
   constructor(private readonly fileUploadService: FileUploadService) {}
 
   @ApiOperation({ summary: 'Presigned URL 요청', description: 'AWS S3에 파일을 업로드하기 위한 presigned URL을 요청합니다.' })
-  @ApiBody({
-    description: '파일명과 파일 타입을 전달하여 presigned URL을 생성합니다.',
-    type: PresignedUrlDto,
-  })
-  @ApiResponse({
-    status: 200,
-    description: '성공적으로 presigned URL을 반환합니다.',
-    schema: {
-      example: {
-        presignedURL: 'https://example-presigned-url.com',
-      },
-    },
-  })
-  @ApiResponse({
-    status: 400,
-    description: '잘못된 요청입니다.',
-  })  
+  @ApiBody({ description: '파일명과 파일 타입을 전달하여 presigned URL을 생성합니다.', type: PresignedUrlDto })
+  @ApiResponse({ status: 200, description: '성공적으로 presigned URL을 반환합니다.', type: PresignedUrlResponseDto })
+  @ApiResponse({ status: 400, description: '잘못된 요청입니다.' })
   @Post()
-  async getPresignedUrl(@Body() body: PresignedUrlDto): Promise<{ presignedURL: string }> {
+  async getPresignedUrl(@Body() body: PresignedUrlDto): Promise<PresignedUrlResponseDto> {
     const presignedURL = await this.fileUploadService.getPresignedUrl(body);
     return { presignedURL };
   }
--- server/src/modules/profile/profile.controller.ts
+++ server/src/modules/profile/profile.controller.ts
@@ -7,13 +7,14 @@
  * Date          Author      Status      Description
  * 2024.09.17    이승철      Created
  * 2024.09.19    이승철      Modified    ApiResponse 추가
+ * 2024.09.21    이승철      Modified    swagger 데코레이터 재정렬
  */
 
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
-import { ProfileService } from './profile.service';
-import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { ProfileDto } from '@_modules/user/dto/profile.dto';
 import { Jwt } from '@_modules/auth/guards/jwt';
+import { ProfileDto } from '@_modules/user/dto/profile.dto';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ProfileService } from './profile.service';
 
 @ApiTags('Profile Management')
 @Controller('profile')
@@ -22,20 +23,9 @@ export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
   @ApiOperation({ summary: '다른 유저의 프로필 조회' })
-  @ApiParam({
-    name: 'userId',
-    description: '조회할 유저의 ID',
-    example: 1,
-  })
-  @ApiResponse({
-    status: 200,
-    description: '성공적으로 유저 프로필을 반환합니다.',
-    type: ProfileDto,
-  })
-  @ApiResponse({
-    status: 404,
-    description: '유저를 찾을 수 없습니다.',
-  })
+  @ApiParam({ name: 'userId', description: '조회할 유저의 ID', example: 1 })
+  @ApiResponse({ status: 200, description: '성공적으로 유저 프로필을 반환합니다.', type: ProfileDto })
+  @ApiResponse({ status: 404, description: '유저를 찾을 수 없습니다.' })
   @Get(':userId')
   async getUserProfile(@Param('userId') userId: number): Promise<ProfileDto> {
     return this.profileService.getUserProfile(userId);
