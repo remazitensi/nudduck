e4bf8d5b1fc744d7c95f44ca4e23a69d236d468a
remaziTensi <guming0104@gmail.com>
2024-09-22 04:07:06 +0900

    :recycle: Refactor: 커뮤니티 엔티티에서 유저 정보 처리 방식 변경

--- server/src/modules/community/entities/comment.entity.ts
+++ server/src/modules/community/entities/comment.entity.ts
@@ -9,48 +9,41 @@
  * 2024.09.10    김재영      Modified    typeorm 추가
  * 2024.09.13    김재영      Modified    대댓글 개수 추가
  * 2024.09.17    김재영      Modified    주석 업데이트 및 설명 추가
- * 2024.09.19    김재영      Modified    오타 수정 및 유저 추가
+ * 2024.09.19    김재영      Modified    오타 수정
+ * 2024.09.20    김재영      Modified    jwt 가드에서 받아오므로 유저 삭제
  */
 
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany, CreateDateColumn, UpdateDateColumn } from 'typeorm';
-import { Community } from './community.entity';
-import { User } from '@_modules/user/entity/user.entity';
+import { Community } from '@_modules/community/entities/community.entity';
 
-@Entity('comment') // 테이블 이름 지정
+@Entity('comment')
 export class Comment {
-  @PrimaryGeneratedColumn() // 자동 생성되는 PK
+  @PrimaryGeneratedColumn()
   id: number;
 
-  @Column('text') // 긴 텍스트를 위한 컬럼
-  content: string; // 댓글 내용
+  @Column('text')
+  content: string;
 
-  @Column({ nullable: true }) // nullable로 대댓글이 아닐 경우 null 허용
-  parentId?: number | null; // 상위 댓글의 ID (대댓글일 경우)
-
-  @ManyToOne(() => User, (user) => user.comments) // 댓글 작성자
-  user: User;
+  @Column({ nullable: true })
+  parentId?: number | null;
 
   @Column() // 게시글 ID
   postId: number;
 
-  @CreateDateColumn({ type: 'timestamp' }) // 자동 생성되는 생성일 컬럼
+  @CreateDateColumn({ type: 'timestamp' })
   createdAt: Date;
 
-  @UpdateDateColumn({ type: 'timestamp' }) // 자동 수정되는 수정일 컬럼
+  @UpdateDateColumn({ type: 'timestamp' })
   updatedAt: Date;
 
-  // 댓글이 속한 커뮤니티 게시글
   @ManyToOne(() => Community, (community) => community.comments, { onDelete: 'CASCADE' })
   community: Community;
 
-  // 부모 댓글
   @ManyToOne(() => Comment, (comment) => comment.replies, { nullable: true, onDelete: 'CASCADE' })
   parent?: Comment;
 
-  // 대댓글 배열 (self-referencing relationship)
   @OneToMany(() => Comment, (comment) => comment.parent)
   replies?: Comment[];
 
-  // 대댓글 개수 계산
-  replyCount?: number; // 실제로 DB에 저장되지 않고, 조회할 때 계산
+  replyCount?: number;
 }
