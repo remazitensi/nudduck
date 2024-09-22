b51db5e0bcfadd5aa1b8fd5290de09915c7c7184
remazitensi <guming0104@gmail.com>
2024-09-22 17:11:53 +0900

    ♻️ Refactor: 댓글 응답 DTO 향상 - 데이터 표현 개선

--- server/src/modules/community/dto/response/comment-response.dto.ts
+++ server/src/modules/community/dto/response/comment-response.dto.ts
@@ -5,11 +5,20 @@
  *
  * History
  * Date          Author      Status      Description
- * 2024.09.22    김재영      Created     댓글 응답 DTO 초기 생성
+ * 2024.09.21    김재영      Created     댓글 응답 DTO 초기 생성
+ * 2024.09.22    김재영      Modified    생성자 추가 및 데이터 매핑 로직 추가
  */
+
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Comment } from '@_modules/community/entities/comment.entity';
 
 export class CommentResponseDto {
+  @ApiPropertyOptional({
+    description: '게시글 ID',
+    example: 10,
+  })
+  postId?: number;
+
   @ApiProperty({
     description: '댓글 ID',
     example: 101,
@@ -34,21 +43,46 @@ export class CommentResponseDto {
   })
   updatedAt: Date;
 
-  @ApiProperty({
+  @ApiPropertyOptional({
     description: '대댓글 수',
     example: 2,
   })
-  replyCount: number;
+  replyCount?: number;
 
   @ApiPropertyOptional({
     description: '대댓글일 경우 부모 댓글의 ID',
     example: 1,
   })
-  parentId?: number; // 대댓글일 경우, 부모 댓글 ID
+  parentId?: number;
 
-  @ApiProperty({ description: '작성자 닉네임', example: '멋쨍이째영' })
-  readonly nickname: string;
+  @ApiProperty({
+    description: '작성자 ID',
+    example: 101,
+  })
+  userId: number;
 
-  @ApiProperty({ description: '작성자 프로필 사진 URL', example: 'http://nudduck.com/remazitensi.jpg' })
-  readonly imageUrl?: string;
+  @ApiProperty({
+    description: '작성자 닉네임',
+    example: '멋쨍이째영',
+  })
+  nickname: string;
+
+  @ApiPropertyOptional({
+    description: '작성자 프로필 사진 URL',
+    example: 'http://nudduck.com/remazitensi.jpg',
+  })
+  imageUrl?: string;
+
+  constructor(comment: Comment, replyCount?: number) {
+    this.postId = comment.postId; // 커뮤니티 ID
+    this.commentId = comment.id; // 댓글 ID
+    this.content = comment.content; // 댓글 내용
+    this.createdAt = comment.createdAt; // 작성 일시
+    this.updatedAt = comment.updatedAt; // 수정 일시
+    this.replyCount = replyCount; // 대댓글 수
+    this.parentId = comment.parentId; // 부모 댓글 ID
+    this.userId = comment.user?.id || null; // 작성자 ID
+    this.nickname = comment.user?.nickname || ''; // 작성자 닉네임
+    this.imageUrl = comment.user?.image_url || ''; // 작성자 프로필 사진 URL
+  }
 }
