90a5fbefcfb48a19a4ca4e7cbefca2bfdbac1b9f
remaziTensi <guming0104@gmail.com>
2024-09-22 17:12:00 +0900

    ♻️ Refactor: 커뮤니티 응답 DTO 업데이트 - 추가 필드 포함

--- server/src/modules/community/dto/response/community-response.dto.ts
+++ server/src/modules/community/dto/response/community-response.dto.ts
@@ -7,9 +7,9 @@
  * Date          Author      Status      Description
  * 2024.09.20    김재영      Created     커뮤니티 게시글 DTO 생성
  */
-
 import { ApiProperty } from '@nestjs/swagger';
 import { Category } from '@_modules/community/enums/category.enum';
+import { Community } from '@_modules/community/entities/community.entity';
 
 export class CommunityResponseDto {
   @ApiProperty({
@@ -48,4 +48,18 @@ export class CommunityResponseDto {
 
   @ApiProperty({ description: '작성자 프로필 사진 URL', example: 'http://nudduck.com/remazitensi.jpg' })
   readonly imageUrl?: string;
+
+  @ApiProperty({ description: '작성자 ID', example: 1 })
+  readonly userId: number; // 작성자 ID 추가
+
+  constructor(community: Community) {
+    this.postId = community.postId;
+    this.title = community.title;
+    this.viewCount = community.viewCount;
+    this.createdAt = community.createdAt;
+    this.category = community.category;
+    this.userId = community.user?.id || null; // userId 할당
+    this.nickname = community.user.nickname; // 작성자 닉네임
+    this.imageUrl = community.user.image_url; // 작성자 프로필 사진 URL
+  }
 }
