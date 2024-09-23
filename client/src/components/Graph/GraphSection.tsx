339cc4000759c1df192651289bdcd8636cf4ee34
[AI11] 김민지 <kkimming__@naver.com>
2024-09-24 07:36:09 +0900

    ✨ Feat: 그래프 즐겨찾기, 조회

--- client/src/components/Graph/GraphSection.tsx
+++ client/src/components/Graph/GraphSection.tsx
@@ -6,52 +6,42 @@
  * History
  * Date          Author      Status      Description
  * 2024.09.22    김민지      Created     그래프 섹션 컴포넌트 분리, 차트 이름 추가, 그래프 연결
+ * 2024.09.24    김민지      Modified     그래프 리스트 get, 즐겨찾기, 그래프 추가/삭제
  */
 
-import { useState } from 'react';
 import { GraphData } from '../../types/graph-type';
 import { CreateListGraph } from './CreateListGraph';
 
-export const GraphSection = ({ title, created_at, updated_at, events }: GraphData) => {
-  const [star, setStar] = useState('grayStar.png');
+const GraphSection = ({ title, createdAt, updatedAt, events, id, activeStarId, changeActiveStar }: GraphData) => {
+  const isActive = activeStarId === id; // 현재 그래프가 활성화된 스타인지 확인
 
   const changeStar = () => {
-    setStar((prev) => (prev === 'grayStar.png' ? 'yellowStar.png' : 'grayStar.png'));
+    changeActiveStar(isActive ? null : id); // 클릭 시 활성화 상태 변경
   };
 
   return (
     <div className='flex w-[380px] flex-col'>
       <div className='mb-[5px] flex justify-end gap-[10px]'>
-        <div className='flex h-[30px] w-[80px] cursor-pointer items-center justify-center gap-[10px] rounded-[5px] bg-[#FFEABA]'>
-          <img src='Edit.png' className='h-[25px] w-[25px]' />
-          {/* todo : 수정 이벤트 */}
-          <button className='flex text-center'>수정</button>
-        </div>
-        <div className='flex h-[30px] w-[80px] cursor-pointer items-center justify-center gap-[10px] rounded-[5px] bg-[#FFEABA]'>
-          <img src='delete.png' className='h-[25px] w-[25px]' />
-          {/* Todo : 삭제 이벤트 */}
-          <button className='flex text-center'>삭제</button>
-        </div>
+        <img src='/edit-btn.png' className='cursor-pointer' />
+        <img src='/delete-btn.png' className='cursor-pointer' />
       </div>
       <div className='flex w-[380px] bg-[#F8F8F8] hover:shadow-md'>
         <div className='mt-[30px]'>
-          {/* 그래프 컴포넌트 연결 완료 */}
           <div className='h-[220px] w-[380px]'>
             <CreateListGraph events={events}></CreateListGraph>
           </div>
           <div className=''>
-            <div className='flex items-center justify-center gap-[160px] pl-[15px]'>
-              {/* todo : 즐겨찾기 하나 선택하면 다른 하나 풀리게 */}
-              <img src={`/${star}`} onClick={changeStar} className='cursor-pointer' />
-              <div className='p-[5px]'>
+            <div className='flex items-center justify-between pl-[15px]'>
+              <img src={`/${isActive ? 'yellowStar.png' : 'grayStar.png'}`} onClick={changeStar} className='cursor-pointer' />
+              <div className='p-[5px] text-right'>
                 <div className='text-[#8D8B67]'>{title}</div>
-                <div className='flex gap-[20px] text-[#8D8B67]'>
+                <div className='flex justify-end gap-[20px] text-[#8D8B67]'>
                   <div>생성일</div>
-                  <div>{created_at.slice(0, 10)}</div>
+                  <div>{createdAt.slice(0, 10)}</div>
                 </div>
-                <div className='flex gap-[20px]'>
+                <div className='flex justify-end gap-[20px]'>
                   <div>수정일</div>
-                  <div>{updated_at.slice(0, 10)}</div>
+                  <div>{updatedAt.slice(0, 10)}</div>
                 </div>
               </div>
             </div>
@@ -61,3 +51,5 @@ export const GraphSection = ({ title, created_at, updated_at, events }: GraphDat
     </div>
   );
 };
+
+export default GraphSection;
