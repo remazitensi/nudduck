794080a40d5f2bc8f20a3c54ad736e960c83ca5f
remaziTensi <guming0104@gmail.com>
2024-09-24 12:35:26 +0900

    :goal: Catch: 명언 및 영어 문장 조회 시 예외 처리 추가

--- server/src/modules/quote/schedule.service.ts
+++ server/src/modules/quote/schedule.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import { Quote } from './entities/quote.entity';
-import { EnglishSentence } from './entities/english-sentence.entity';
-import { QuoteDto } from './dto/quote.dto';
-import { EnglishSentenceDto } from './dto/english-sentence.dto';
+import { Quote } from '@_modules/Quote/entities/quote.entity';
+import { EnglishSentence } from '@_modules/Quote/entities/english-sentence.entity';
+import { QuoteDto } from '@_modules/Quote/dto/quote.dto';
+import { EnglishSentenceDto } from '@_modules/Quote/dto/english-sentence.dto';
 
 @Injectable()
 export class ScheduleService {
@@ -19,6 +19,9 @@ export class ScheduleService {
   // Quotes 데이터 조회
   async findAllQuotes(): Promise<QuoteDto[]> {
     const quotes = await this.quotesRepository.find();
+    if (!quotes.length) {
+      throw new HttpException('명언을 찾을 수 없습니다.', HttpStatus.NOT_FOUND);
+    }
     return quotes.map((quote) => ({
       id: quote.id,
       author: quote.author,
@@ -30,6 +33,9 @@ export class ScheduleService {
   // EnglishSentences 데이터 조회
   async findAllEnglishSentences(): Promise<EnglishSentenceDto[]> {
     const sentences = await this.englishSentencesRepository.find();
+    if (!sentences.length) {
+      throw new HttpException('영어 문장을 찾을 수 없습니다.', HttpStatus.NOT_FOUND);
+    }
     return sentences.map((sentence) => ({
       id: sentence.id,
       english: sentence.english,
@@ -38,10 +44,13 @@ export class ScheduleService {
     }));
   }
 
-  // 매일 오전 9시에 Quotes 조회
-  @Cron(CronExpression.EVERY_DAY_AT_9AM) // 매일 9시에 실행
+  // 매일 오전 7시에 Quotes 조회
+  @Cron(CronExpression.EVERY_DAY_AT_7AM) // 매일 9시에 실행
   async handleCron() {
     const quotes = await this.findAllQuotes();
-    console.log('Quotes at 9 AM:', quotes); // 데이터를 활용하는 로직 추가
+    if (!quotes) {
+      throw new HttpException('명언을 가져오는 도중 오류가 발생했습니다.', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    throw new HttpException('명언을 성공적으로 가져왔습니다.', HttpStatus.OK);
   }
 }
