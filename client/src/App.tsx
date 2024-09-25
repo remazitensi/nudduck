8e0d7a4c9c877f862be38e7c0dc9a1b36987a113
[AI11] 김민지 <kkimming__@naver.com>
2024-09-25 12:48:09 +0900

    🚧 WIP : 전문가 테스트

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,7 @@
 //  */
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 // TODO: Layout 컴포넌트 만들기!
 
 // import Layout from './layout/Layout'; // Layout 컴포넌트 임포트
@@ -28,6 +28,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import ExpertsPage from './pages/ExpertsPage';
 // import LifeGraphDetail from './pages/LifeGraphDetail';
 // import LifeGraphList from './pages/LifeGraphList';
+import ExpertsPage from './pages/ExpertsPage';
 import MyPage from './pages/MyPage';
 
 // import CustomerSupportPage from './pages/CustomerSupportPage';
@@ -40,51 +41,51 @@ const App: React.FC = () => {
       <Routes>
         {/* Layout을 사용하여 Header와 Footer를 모든 페이지에 적용 */}
         {/* <Route path='/' element={<Layout />}>  */}
-          {/* 메인 페이지 */}
-          {/* <Route index element={<MainPage />} /> */}
+        {/* 메인 페이지 */}
+        {/* <Route index element={<MainPage />} /> */}
 
-          {/* 홈페이지 */}
-          {/* <Route path='home' element={<HomePage />} /> */}
+        {/* 홈페이지 */}
+        {/* <Route path='home' element={<HomePage />} /> */}
 
-          {/* 커뮤니티 관련 라우트 */}
-          <Route path='community'>
-            {/* 기본으로 게시글 리스트를 렌더링 */}
-            {/* <Route index element={<CommunityPostList />} /> */}
-            {/* 게시글 상세 페이지 */}
-            {/* <Route path=':id' element={<CommunityPostDetail />} /> */}
-            {/* 게시글 작성 페이지 */}
-            {/* <Route path='create' element={<CommunityPostCreate />} /> */}
-            {/* 게시글 수정 페이지 */}
-            {/* <Route path='edit/:id' element={<CommunityPostEdit />} /> */}
-          </Route>
+        {/* 커뮤니티 관련 라우트 */}
+        <Route path='community'>
+          {/* 기본으로 게시글 리스트를 렌더링 */}
+          {/* <Route index element={<CommunityPostList />} /> */}
+          {/* 게시글 상세 페이지 */}
+          {/* <Route path=':id' element={<CommunityPostDetail />} /> */}
+          {/* 게시글 작성 페이지 */}
+          {/* <Route path='create' element={<CommunityPostCreate />} /> */}
+          {/* 게시글 수정 페이지 */}
+          {/* <Route path='edit/:id' element={<CommunityPostEdit />} /> */}
+        </Route>
 
-          {/* 1대1 채팅방 */}
-          {/* <Route path='chat' element={<ChatRoom />} /> */}
+        {/* 1대1 채팅방 */}
+        {/* <Route path='chat' element={<ChatRoom />} /> */}
 
-          {/* AI 코치 */}
-          {/* <Route path="ai-coach/:id?" element={<AICoach />} /> */}
-          
-          {/* 인생 그래프 관련 라우트 */}
-          <Route path='life-graph'>
-            {/* 기본 경로에서 표시될 컴포넌트 */}
-            {/* <Route index element={<LifeGraphList />} /> */}
-            {/* <Route path='detail/:id' element={<LifeGraphDetail />} /> */}
-          </Route>
+        {/* AI 코치 */}
+        {/* <Route path="ai-coach/:id?" element={<AICoach />} /> */}
 
-          {/* 마이페이지 관련 라우트 */}
-          <Route path='my-page' element={<MyPage />} />
+        {/* 인생 그래프 관련 라우트 */}
+        <Route path='life-graph'>
+          {/* 기본 경로에서 표시될 컴포넌트 */}
+          {/* <Route index element={<LifeGraphList />} /> */}
+          {/* <Route path='detail/:id' element={<LifeGraphDetail />} /> */}
+        </Route>
 
-          {/* 전문가 상담 페이지 */}
-          {/* <Route path='experts' element={<ExpertsPage />} /> */}
+        {/* 마이페이지 관련 라우트 */}
+        <Route path='my-page' element={<MyPage />} />
 
-          {/* 고객센터 페이지 */}
-          {/* <Route path='support' element={<CustomerSupportPage />} /> */}
+        {/* 전문가 상담 페이지 */}
+        <Route path='experts' element={<ExpertsPage />} />
 
-          {/* 401 Unauthorized 페이지 */}
-          {/* <Route path='401' element={<Unauthorized />} /> */}
+        {/* 고객센터 페이지 */}
+        {/* <Route path='support' element={<CustomerSupportPage />} /> */}
 
-          {/* 404 Not Found 페이지 */}
-          {/* <Route path='*' element={<NotFound />} /> */}
+        {/* 401 Unauthorized 페이지 */}
+        {/* <Route path='401' element={<Unauthorized />} /> */}
+
+        {/* 404 Not Found 페이지 */}
+        {/* <Route path='*' element={<NotFound />} /> */}
         {/* </Route> */}
       </Routes>
     </Router>
---
+++ client/src/apis/expertspage-api.ts
@@ -0,0 +1,58 @@
+/*
+ * File Name    : experts-api.ts
+ * Description  : experts-api 설정
+ * Author       : 임형선
+ *
+ * History
+ * Date          Author      Status      Description
+ * 2024.09.25    임형선      Created
+ */
+
+import { isAxiosError } from 'axios';
+import { api, baseApi } from './base-api';
+
+interface ExpertsProfile {
+  id: number;
+  name: string;
+  jobTitle: string;
+  age: number;
+  bio: string;
+  profileImage: string;
+  email: string;
+  phone: string;
+  cost: number;
+  hashtags: string;
+  totalCount: number;
+}
+
+// 전문가 리스트 가져오기
+export async function fetchExperts(page: number = 1, limit: number = 10) {
+  const url = `${api.expert}?page=${page}&limit=${limit}`;
+  try {
+    const response = await baseApi.get<{ data: ExpertsProfile[]; totalCount: number }>(url);
+    return response.data;
+  } catch (error) {
+    if (isAxiosError(error)) {
+      console.error('Failed to fetch experts:', error.response?.data.message || error.message);
+    } else {
+      console.error('An unknown error occurred');
+    }
+    throw error;
+  }
+}
+
+// 전문가 상세 정보 가져오기
+export async function fetchExpertDetails(id: number) {
+  const url = `${api.expert}/${id}`;
+  try {
+    const response = await baseApi.get<ExpertsProfile>(url);
+    return response.data;
+  } catch (error) {
+    if (isAxiosError(error)) {
+      console.error('Failed to fetch expert details:', error.response?.data.message || error.message);
+    } else {
+      console.error('An unknown error occurred');
+    }
+    throw error;
+  }
+}
