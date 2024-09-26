f3a99935aca0b5a170f4d04b4f74dc1e2715d88a
remazitensi <guming0104@gmail.com>
2024-09-26 22:11:33 +0900

    🐛 Fix: 게시글 Injectable 수정

--- server/src/modules/community/repositories/community.repository.ts
+++ server/src/modules/community/repositories/community.repository.ts
@@ -1,22 +1,34 @@
-import { Repository, QueryFailedError } from 'typeorm';
-import { Community } from '@_modules/community/entities/community.entity';
-import { PaginationQueryDto } from '@_modules/community/dto/request/pagination-query.dto';
-import { HttpException, HttpStatus } from '@nestjs/common';
-import { CommunityResponseDto } from '@_modules/community/dto/response/community-response.dto';
 import { CreateCommunityDto } from '@_modules/community/dto/request/create-community.dto';
+import { PaginationQueryDto } from '@_modules/community/dto/request/pagination-query.dto';
 import { UpdateCommunityDto } from '@_modules/community/dto/request/update-community.dto';
+import { CommunityResponseDto } from '@_modules/community/dto/response/community-response.dto';
+import { Community } from '@_modules/community/entities/community.entity';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { DataSource, QueryFailedError, Repository } from 'typeorm';
+import { Category } from '../enums/category.enum';
 
+@Injectable()
 export class CommunityRepository extends Repository<Community> {
+  constructor(dataSource: DataSource) {
+    super(Community, dataSource.createEntityManager());
+  }
+
   private buildPaginationQuery(paginationQuery: PaginationQueryDto) {
-    const { page = 1, pageSize = 10, sort = 'createdAt:DESC' } = paginationQuery;
+    const page = paginationQuery.page || 1;
+    const pageSize = paginationQuery.pageSize || 10;
+    const sort = paginationQuery.sort || 'createdAt:DESC';
     const take = pageSize;
     const skip = (page - 1) * pageSize;
 
     const [sortField, sortOrder] = sort.split(':');
     const validSortFields = ['createdAt', 'viewCount'];
+    const validSortOrders = ['ASC', 'DESC'];
 
     if (!validSortFields.includes(sortField)) {
       throw new HttpException('유효하지 않은 정렬 필드입니다.', HttpStatus.BAD_REQUEST);
+    }
+    if (!validSortOrders.includes(sortOrder)) {
+      throw new HttpException('유효하지 않은 정렬 순서입니다.', HttpStatus.BAD_REQUEST);
     }
 
     return { take, skip, sortField, sortOrder };
@@ -28,7 +40,7 @@ export class CommunityRepository extends Repository<Community> {
     try {
       const [posts, total] = await this.createQueryBuilder('community')
         .leftJoinAndSelect('community.user', 'user')
-        .select(['community.postId', 'community.title', 'community.viewCount', 'community.createdAt', 'community.category', 'user.nickname', 'user.image_url'])
+        .select(['community.postId', 'community.title', 'community.viewCount', 'community.createdAt', 'community.category', 'user.id', 'user.nickname', 'user.imageUrl'])
         .orderBy(`community.${sortField}`, sortOrder as 'ASC' | 'DESC')
         .take(take)
         .skip(skip)
@@ -38,38 +50,41 @@ export class CommunityRepository extends Repository<Community> {
 
       return [postsDto, total];
     } catch (error) {
-      this.handleError(error);
+      throw new HttpException(`게시글 조회 중 오류 발생: ${error.message}`, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
-  async findByCategory(category: string, paginationQuery: PaginationQueryDto): Promise<[CommunityResponseDto[], number]> {
+  async findByCategory(category: Category, paginationQuery: PaginationQueryDto): Promise<[CommunityResponseDto[], number]> {
     const { take, skip, sortField, sortOrder } = this.buildPaginationQuery(paginationQuery);
 
     try {
-      const [posts, total] = await this.createQueryBuilder('community')
+      const queryBuilder = this.createQueryBuilder('community')
         .leftJoinAndSelect('community.user', 'user')
         .where('community.category = :category', { category })
-        .select(['community.postId', 'community.title', 'community.viewCount', 'community.createdAt', 'community.category', 'user.nickname', 'user.image_url'])
+        .select(['community.postId', 'community.title', 'community.viewCount', 'community.createdAt', 'community.category', 'user.id', 'user.nickname', 'user.imageUrl'])
         .orderBy(`community.${sortField}`, sortOrder as 'ASC' | 'DESC')
         .take(take)
-        .skip(skip)
-        .getManyAndCount();
+        .skip(skip);
+
+      const [posts, total] = await queryBuilder.getManyAndCount();
 
       const postsDto = posts.map((post) => new CommunityResponseDto(post));
 
       return [postsDto, total];
     } catch (error) {
-      this.handleError(error);
+      throw new HttpException(`카테고리별 게시글 조회 중 오류 발생: ${error.message}`, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
   async createPost(postData: CreateCommunityDto): Promise<Community> {
     const newPost = this.create({
       ...postData,
-      user: { id: postData.userId }, // userId 추가
+      user: { id: postData.userId },
     });
+
     try {
-      return await this.save(newPost);
+      const savedPost = await this.save(newPost);
+      return savedPost;
     } catch (error) {
       this.handleError(error);
     }
@@ -103,6 +118,7 @@ export class CommunityRepository extends Repository<Community> {
     if (error instanceof QueryFailedError) {
       throw new HttpException('쿼리 오류 발생', HttpStatus.INTERNAL_SERVER_ERROR);
     }
+    console.error('Error in handleError:', error);
     throw new HttpException('서버 오류 발생', HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
