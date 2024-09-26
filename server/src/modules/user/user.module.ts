f6a602970d75764edc0790f19bf0bff9ff0735c7
l66612472@gmail.com <l66612472@gmail.com>
2024-09-27 03:18:36 +0900

    :recycle: Refactor: 순환참조 제거 및 비동기 병렬처리

--- server/src/modules/user/user.module.ts
+++ server/src/modules/user/user.module.ts
@@ -10,21 +10,22 @@
  * 2024.09.21    이승철      Modified    lifeGraph 모듈 주입
  * 2024.09.24    이승철      Modified    community 모듈 주입
  * 2024.09.26    이승철      Modified    community 모듈 주입 수정
+ * 2024.09.27    이승철      Modified    순환참조 제거
  */
 
 import { Community } from '@_modules/community/entities/community.entity';
 import { FileUploadModule } from '@_modules/file-upload/file-upload.module';
-import { LifeGraphModule } from '@_modules/life-graph/life-graph.module';
+import { LifeGraph } from '@_modules/life-graph/entity/life-graph.entity';
 import { UserHashtag } from '@_modules/user/entity/hashtag.entity';
 import { User } from '@_modules/user/entity/user.entity';
 import { UserController } from '@_modules/user/user.controller';
 import { UserRepository } from '@_modules/user/user.repository';
 import { UserService } from '@_modules/user/user.service';
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, UserHashtag, Community]), FileUploadModule, forwardRef(() => LifeGraphModule)],
+  imports: [TypeOrmModule.forFeature([User, UserHashtag, Community, LifeGraph]), FileUploadModule],
   controllers: [UserController],
   providers: [UserService, UserRepository],
   exports: [UserService, UserRepository],
