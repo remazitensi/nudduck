d90edc5f521c9d0c1d9dcf54c829936d8ad79abc
[AI11] 김민지 <kkimming__@naver.com>
2024-09-28 16:09:05 +0900

    ✨ Feat: 게시글 목록에서 유저 프로필 모달 open

--- client/src/components/Community/PostSection.tsx
+++ client/src/components/Community/PostSection.tsx
@@ -10,8 +10,10 @@
  * 2024.09.19    김민지      Modified    게시글 상세페이지 이동, 타입에러 해결, 파일명 변경, CategoryBtn 컴포넌트 분리
  */
 
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { PostTitleData } from '../../types/community-type';
+import AnotherUserModal from './AnotherUserModal';
 import { CategoryBtn } from './CategoryBtn';
 
 type PostSectionProps = {
@@ -20,42 +22,54 @@ type PostSectionProps = {
 
 export const PostSection: React.FC<PostSectionProps> = ({ data }) => {
   const navigate = useNavigate();
+  const [openUserModal, setOpenUserModal] = useState(false);
 
   // 클릭 시 해당 게시글  페이지로 이동
   const handleRoute = () => {
     navigate(`/community/${data.postId}`);
   };
 
+  const handleOpenModal = () => {
+    setOpenUserModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenUserModal(false);
+  };
+
   return (
     <div className='mt-[15px] border-b-2 border-[8D8B67]'>
-      <div className='flex w-full items-center gap-[70px]'>
+      {openUserModal && <AnotherUserModal onClose={handleCloseModal} userId={data.userId} />}
+
+      <div className='flex w-full items-center gap-[20px]'>
         <CategoryBtn category={data.category} />
-        <div className='cursor-pointer text-[20px] hover:font-bold' onClick={handleRoute}>
+        <div className='cursor-pointer p-[20px] text-[20px] hover:font-bold' onClick={handleRoute}>
           {data.title}
         </div>
         <div className='ml-auto'>
-          <div className='mb-[5px] flex items-center justify-end gap-[5px]'>
-            <div className='h-[28px] w-[28px] rounded-full'>
-              <img className='rounded-full object-cover' src={data.imageUrl ? data.imageUrl : '/default-img.png'} alt='profile_Img' />
+          <div className='mb-[5px] flex items-center justify-end'>
+            <div className='flex gap-[5px] p-[5px]' onClick={handleOpenModal}>
+              <div className='h-[28px] w-[28px] rounded-full'>
+                <img className='rounded-full object-cover' src={data.imageUrl ? data.imageUrl : '/default-img.png'} alt='profile_Img' />
+              </div>
+              <div>{data.nickname}</div>
             </div>
-            <div>{data.nickname}</div>
           </div>
-        </div>
-      </div>
-
-      <div className='flex justify-end text-[16px]'>
-        <div className='flex items-center'>
-          <div className='flex items-center text-[#AEAC9A]'>
-            조회수
-            <span className='min-w-[40px] text-right text-[#A1DFFF]'>{data.viewCount}</span>
-          </div>
-          {/* 좋아요 삭제 */}
-          {/* <div className='ml-[30px] text-[#AEAC9A]'>
+          <div className='flex justify-end text-[16px]'>
+            <div className='flex items-center'>
+              <div className='flex items-center text-[#AEAC9A]'>
+                조회수
+                <span className='min-w-[40px] text-right text-[#A1DFFF]'>{data.viewCount}</span>
+              </div>
+              {/* 좋아요 삭제 */}
+              {/* <div className='ml-[30px] text-[#AEAC9A]'>
       좋아요 <span className='text-[#FFC5C3]'>{data.likes_count}</span>
     </div> */}
-          <div className='ml-[20px] text-[#AEAC9A]'>
-            작성일
-            <span className='min-w-[100px] text-right'>{data.createdAt.substring(0, 10)}</span>
+              <div className='ml-[20px] text-[#AEAC9A]'>
+                작성일
+                <span className='min-w-[100px] text-right'>{data.createdAt.substring(0, 10)}</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
