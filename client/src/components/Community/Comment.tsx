3ce10ddae0e2e184f09492c3afac22379f1b1e06
[AI11] 김민지 <kkimming__@naver.com>
2024-09-30 04:37:08 +0900

    ✨ Feat: user 정보 조회 api,  userId와 작성자 Id 비교해서 수정/삭제 리턴

--- client/src/components/Community/Comment.tsx
+++ client/src/components/Community/Comment.tsx
@@ -11,20 +11,28 @@
 import { changeDateWithFormat } from '../../apis/change-date-format';
 import { CommentsDto } from '../../types/comments-type';
 
-export const Comment: React.FC<{ comment: CommentsDto }> = ({ comment }) => {
+export const Comment: React.FC<{ comment: CommentsDto; isWriter: boolean }> = ({ comment, isWriter }) => {
   const createdAt = changeDateWithFormat(comment.createdAt);
-  // console.log(comment.createdAt, '--->', createdAt); // 날짜 변경 확인
 
   return (
     <div>
-      <div className='flex gap-[10px] pl-[20px] pt-[20px]'>
-        <div className='h-[50px] w-[50px] rounded-full'>
-          <img className='object-cover' src={comment.imageUrl} alt='profile-img' />
+      <div className='flex items-center justify-between'>
+        <div className='flex items-center gap-[10px] p-[10px]'>
+          <div className='h-[50px] w-[50px] rounded-full'>
+            <img className='object-cover' src={comment.imageUrl} alt='profile-img' />
+          </div>
+          <div className='flex items-center text-[20px] font-semibold'>{comment.nickname}</div>
+          <div className='flex items-center text-[12px] text-[#AEAC9A]'>{createdAt}</div>
+          <div className='flex items-center p-[5px] text-[16px] font-semibold text-[#AEAC9A] hover:text-[#909700]'>대댓글 달기</div>
         </div>
-        <div className='flex items-center text-[20px] font-semibold'>{comment.nickname}</div>
-        <div className='flex items-center text-[12px] text-[#AEAC9A]'>{createdAt}</div>
+        {isWriter && (
+          <div className='flex items-center gap-[10px] px-[10px]'>
+            <div className='flex items-center p-[5px] text-[12px] text-[#AEAC9A]'>수정</div>
+            <div className='flex items-center p-[5px] text-[12px] text-[#AEAC9A]'>삭제</div>
+          </div>
+        )}
       </div>
-      <div className='pl-[20px] pt-[7px] text-[16px]'>{comment.content}</div>
+      <div className='pl-[68px] text-[16px]'>{comment.content}</div>
     </div>
   );
 };
--- client/src/components/Community/CommentThread.tsx
+++ client/src/components/Community/CommentThread.tsx
@@ -14,15 +14,16 @@ import { CommentsDto } from '../../types/comments-type';
 import { Comment } from './Comment';
 import { Reply } from './Reply';
 
-export const CommentThread: React.FC<{ comment: CommentsDto }> = ({ comment }) => {
+export const CommentThread: React.FC<{ comment: CommentsDto; userId: number }> = ({ comment, userId }) => {
   // Reply 컴포넌트의 표시 여부를 관리하는 상태
   const [isReplyVisible, setIsReplyVisible] = useState(false);
   const [replies, setReplies] = useState<CommentsDto[]>([]);
 
+  // isWriter 계산을 한 번만 수행
+  const isWriter = comment.userId === userId;
+
   // Reply 컴포넌트 표시/숨기기 토글 함수
   const toggleReplyVisibility = async () => {
-    // 고도화 : 세션스토리지에서 부모댓글Id를 조회해서 있으면 그거 쓰기
-
     if (!isReplyVisible && comment.replyCount > 0) {
       // 대댓글이 처음 열릴 때만 API 요청을 보냄
       const replyData = await fetchReplyComment();
@@ -31,30 +32,27 @@ export const CommentThread: React.FC<{ comment: CommentsDto }> = ({ comment }) =
     setIsReplyVisible(!isReplyVisible);
   };
 
-  // 대댓글 조회 api 요청 함수
+  // 대댓글 조회 API 요청 함수
   const fetchReplyComment = async () => {
     const data = await getReply(comment.postId, comment.commentId);
-    // 고도화 : 댓글 데이터를 세션스토리지에 저장?
     return data;
   };
 
   return (
     <div>
       <div className='mt-[35px] w-[1200px] border-b-2 border-[8D8B67]'></div>
+      {/* isWriter 값을 바로 전달 */}
+      <Comment comment={comment} isWriter={isWriter} />
 
-      <Comment comment={comment} />
-
+      {/* 대댓글이 있을 경우에만 토글 표시 */}
       {comment.replyCount > 0 && (
         <div className='flex items-center gap-[10px]'>
-          {/* 댓글이 닫힌 상태일 때 down arrow와 댓글 열기 표시 */}
-          {comment.replyCount !== 0 && !isReplyVisible && (
+          {!isReplyVisible && (
             <div className='flex items-center gap-[10px]' onClick={toggleReplyVisibility}>
               <img src='/down_arrow.png' alt='댓글 열기' />
-              <p>댓글 열기 (${comment.replyCount})</p>
+              <p>댓글 열기 ({comment.replyCount})</p>
             </div>
           )}
-
-          {/* 댓글이 열린 상태일 때 up arrow와 댓글 닫기 표시 */}
           {isReplyVisible && (
             <div className='flex items-center gap-[10px]' onClick={toggleReplyVisibility}>
               <img src='/up_arrow.png' alt='댓글 닫기' />
