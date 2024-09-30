eb9463313ef9afb6cb022646b657921f2f4bd5c8
[AI11] 김민지 <kkimming__@naver.com>
2024-09-30 19:00:44 +0900

    🚧 WIP: 대댓글 조회

--- client/src/components/Community/Comment.tsx
+++ client/src/components/Community/Comment.tsx
@@ -8,12 +8,19 @@
  * 2024.09.28    김민지      Created
  */
 
+import { deleteComment } from '../../apis/community/community-comments-api';
 import { CommentsDto } from '../../types/comments-type';
 import { changeDateWithFormat } from '../../utils/change-date-with-format';
 
-export const Comment: React.FC<{ comment: CommentsDto; isWriter: boolean }> = ({ comment, isWriter }) => {
-  console.log('comment.createdAt:', comment.createdAt); // 확인용
+export const Comment: React.FC<{ comment: CommentsDto; isWriter: boolean; onDelete: () => void; onReply: () => void }> = ({ comment, isWriter, onDelete, onReply }) => {
   const createdAt = changeDateWithFormat(comment.createdAt);
+
+  const handleDeleteComment = async () => {
+    const response = await deleteComment(comment.postId, comment.commentId);
+    if (response.status === 204) {
+      onDelete(); // 상위 컴포넌트에 삭제를 알림
+    }
+  };
 
   return (
     <div>
@@ -24,16 +31,20 @@ export const Comment: React.FC<{ comment: CommentsDto; isWriter: boolean }> = ({
           </div>
           <div className='flex items-center text-[20px] font-semibold'>{comment.nickname}</div>
           <div className='flex items-center text-[12px] text-[#AEAC9A]'>{createdAt}</div>
-          <div className='flex items-center p-[5px] text-[16px] font-semibold text-[#AEAC9A] hover:text-[#909700]'>대댓글 달기</div>
+          <div className='flex cursor-pointer items-center p-[5px] text-[16px] font-semibold text-[#AEAC9A] hover:text-[#909700]' onClick={onReply}>
+            대댓글 달기
+          </div>
         </div>
         {isWriter && (
           <div className='flex items-center gap-[10px] px-[10px]'>
-            <div className='flex items-center p-[5px] text-[12px] text-[#AEAC9A]'>수정</div>
-            <div className='flex items-center p-[5px] text-[12px] text-[#AEAC9A]'>삭제</div>
+            <div className='flex cursor-pointer items-center p-[5px] text-[15px] font-semibold text-[#AEAC9A] hover:text-[#909700]'>수정</div>
+            <div className='flex cursor-pointer items-center p-[5px] text-[15px] font-semibold text-[#AEAC9A] hover:text-[#909700]' onClick={handleDeleteComment}>
+              삭제
+            </div>
           </div>
         )}
       </div>
-      <div className='pl-[68px] text-[16px]'>{comment.content}</div>
+      <div className='pl-[70px] text-[20px]'>{comment.content}</div>
     </div>
   );
 };
--- client/src/components/Community/Reply.tsx
+++ client/src/components/Community/Reply.tsx
@@ -6,26 +6,32 @@
  * History
  * Date          Author      Status      Description
  * 2024.09.28    김민지      Created
+ * 2024.09.30    김민지      Modified    대댓글 데이터 매핑
  */
 
 import { CommentsDto } from '../../types/comments-type';
 
-type ReplyProps = {
-  replies: CommentsDto[];
-};
-
-export const Reply: React.FC<ReplyProps> = ({ replies }) => {
+export const Reply = (replies: CommentsDto[]) => {
+  console.log(replies);
   return (
     <div className='Reply mt-[20px] flex flex-col'>
-      <div className='flex items-center gap-[10px] pl-[20px] pt-[20px]'>
-        {/* 대댓글 화살표 */}
-        <img className='h-[32px] w-[32px]' src='/comment-arrow.png' alt='replyImg' />
-        <img className='' src='/cat_image.png' alt='catImg' />
-        <div className='flex items-center text-[20px] font-semibold'>스터디구하는자</div>
-        <div className='flex items-center text-[12px] text-[#AEAC9A]'>24-09-08 22:22</div>
-      </div>
-      <div className='pl-[60px] pt-[7px] text-[16px]'>안녕하세요 대댓글 입니다</div>
-      <div className='mt-[35px] w-[1200px] border-b-2 border-[8D8B67]'></div>
+      {replies.map((reply, index) => (
+        <div key={index} className='reply-item'>
+          <div className='flex items-center gap-[10px] pl-[20px] pt-[20px]'>
+            {/* 대댓글 화살표 */}
+            <img className='h-[32px] w-[32px]' src='/comment-arrow.png' alt='replyImg' />
+            <img className='' src={reply.imageUrl} alt='userImg' />
+            <div className='flex items-center text-[20px] font-semibold'>{reply.nickname}</div>
+            <div className='flex items-center text-[12px] text-[#AEAC9A]'>{reply.createdAt}</div>
+            <div className='flex items-center gap-[10px] px-[10px]'>
+              <div className='flex items-center p-[5px] text-[12px] text-[#AEAC9A]'>수정</div>
+              <div className='flex items-center p-[5px] text-[12px] text-[#AEAC9A]'>삭제</div>
+            </div>
+          </div>
+          <div className='pl-[60px] pt-[7px] text-[16px]'>{reply.content}</div>
+          <div className='mt-[35px] w-[1200px] border-b-2 border-[8D8B67]'></div>
+        </div>
+      ))}
     </div>
   );
 };
