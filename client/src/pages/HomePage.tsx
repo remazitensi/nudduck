a2defe46c4bfe57cab933cca28cc6db559856d6e
somgyul <namhee9096@gmail.com>
2024-10-01 13:38:22 +0900

    :children_crossing: UX: 메인페이지, 홈페이지 사용자 인터페이스 개선

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -114,7 +114,7 @@ const HomePage = () => {
         const response = await baseApi.get('/community', {
           params: {
             page: 1,
-            pageSize: 6,
+            pageSize: 9,
             limit: 10,
             offset: 0,
             sort: 'viewCount:desc',
@@ -152,22 +152,22 @@ const HomePage = () => {
             onMouseLeave={() => setHoverText(null)}
           >
             <div className='absolute left-0 top-[6px] h-[140px] w-full rounded-[5px] bg-white' />
-            <div className="absolute left-[31px] top-1/2 h-[29px] w-[175px] -translate-y-1/2 transform text-center font-['Pretendard'] text-2xl font-semibold text-[#909700]">
+            <div className="absolute left-[31px] top-1/2 h-[29px] w-[175px] -translate-y-1/2 transform text-center font-['Pretendard'] text-3xl font-semibold text-[#909700]">
               {isQuoteVisible ? '오늘의 명언' : '오늘의 영문장'}
             </div>
-            <div className="absolute left-[222px] top-[50px] text-left font-['Pretendard'] text-base font-normal text-[#313119]">
+            <div className="absolute left-[222px] top-[50px] text-left font-['Pretendard'] text-xl font-normal text-[#313119]">
               {isQuoteVisible && quote ? (
                 <>
-                  <div className='text-base'>"{quote.message}"</div>
-                  <div className='mt-1 text-sm text-gray-500'>
+                  <div className='text-xl'>"{quote.message}"</div>
+                  <div className='mt-1 text-base text-gray-500'>
                     - {quote.author} ({quote.authorProfile})
                   </div>
                 </>
               ) : (
                 englishSentence && (
                   <>
-                    <div>{englishSentence.korean}</div>
-                    <div className='text-sm text-gray-500'>({englishSentence.english})</div>
+                    <div className='text-xl'>{englishSentence.korean}</div>
+                    <div className='text-base text-gray-500'>({englishSentence.english})</div>
                     <div className='mt-1 text-sm italic text-gray-400'>{englishSentence.note}</div>
                   </>
                 )
@@ -191,21 +191,19 @@ const HomePage = () => {
           </div>
         </section>
 
-        {/* 이하 코드는 변경사항 없이 유지됨 */}
-
         {/* AI Coach Section */}
         {!isLoggedIn && (
           <section className='my-6 flex items-center justify-between rounded p-6'>
-            <img src='ai-image1.png' alt='AI Coach' className='h-[600px] w-[700px]' />
+            <img src='nudduck-coach.png' alt='AI Coach' className='h-[600px] w-[700px]' />
             <div className='ml-4 flex flex-col'>
-              <div className='relative rounded-[10px] bg-[#A1DFFF] p-[20px] shadow-xl'>
+              <div className='relative rounded-[10px] bg-[#A1DFFF] p-[20px] shadow-xl transition-transform duration-300 hover:scale-105'>
                 <h3 className='text-xl font-bold text-blue-700'>AI Coach</h3>
                 <p className='mt-2 text-[18px] text-gray-700'>
                   <span className='font-bold'>누떡</span>에서 제공하는 AI 서비스는 사용자가 면접 준비를 효율적으로 할 수 있도록 돕는 혁신적인 도구입니다.
                 </p>
                 <div className='absolute -left-14 top-10 h-0 w-0 border-[30px] border-transparent border-r-[#A1DFFF]'></div>
               </div>
-              <p className='m-auto mt-[95px] text-center text-[24px] font-bold'>
+              <p className='m-auto mt-[95px] text-center text-[24px] font-bold transition-transform duration-300 hover:scale-105'>
                 구글, 카카오 아이디가 있으신가요?
                 <br />
                 <span onClick={openLoginModal} className='cursor-pointer bg-none text-[#909700]'>
@@ -216,15 +214,14 @@ const HomePage = () => {
             </div>
           </section>
         )}
-
-        {/* Feature Cards Section - 로그인 전 상태 */}
+        {/* Feature Cards Section */}
         <section className='mx-auto my-6 max-w-6xl p-6'>
           <h2 className='text-2xl font-bold leading-[30px]'>로그인하고 내 조건에 맞는 서비스 이용하기</h2>
-          <div className='mt-4 grid grid-cols-1 justify-center gap-4 md:grid-cols-4'>
+          <div className='mt-4 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-4'>
             {/* 첫 번째 카드 */}
-            <div className='relative h-[260px] w-[240px] rounded-lg border border-gray-300 bg-[#fbfaec] p-4 shadow transition-all duration-300 hover:border-[#AEAC9A] hover:shadow-lg'>
+            <div className='relative h-[260px] w-full rounded-lg border border-gray-300 bg-[#fbfaec] p-4 transition-all duration-300 hover:border-[#AEAC9A] hover:shadow-lg'>
               <div className='absolute left-[15px] top-[76px] text-sm font-semibold leading-[25px] text-[#aeac9a]'>프로필 채우고 나에게 딱 맞는</div>
-              <div className='absolute left-[15px] top-[100px] text-lg font-bold leading-[25px] text-black'>AI와의 가상 면접을 체험해 보세요!</div>
+              <div className='absolute left-[15px] top-[100px] mr-[4px] text-lg font-bold leading-[25px] text-black'>AI와의 가상 면접을 체험해 보세요!</div>
               <button
                 className='absolute bottom-[12px] left-1/2 h-[35px] w-[200px] -translate-x-1/2 transform rounded bg-[#313119] text-center text-xs font-bold leading-[25px] text-[#aeac9a]'
                 onClick={() => navigate('/my-page')} // 버튼 클릭 시 /my-page로 이동
@@ -234,21 +231,21 @@ const HomePage = () => {
             </div>
 
             {/* 두 번째 카드 */}
-            <div className='relative h-[260px] w-[240px] rounded-lg border border-gray-300 bg-white p-4 shadow transition-all duration-300 hover:border-[#AEAC9A] hover:shadow-lg'>
+            <div className='relative h-[260px] w-full rounded-lg border border-gray-300 bg-white p-4 transition-all duration-300 hover:border-[#AEAC9A] hover:shadow-lg'>
               <img src='ai-image2.png' alt='Expert Consulting' className='absolute left-[68px] h-[150px] w-[150px]' />
               <h3 className='absolute left-[15px] top-[160px] text-base font-bold leading-[25px] text-black'>Expert Consulting</h3>
               <p className='absolute left-[15px] top-[190px] w-[210px] text-sm font-normal leading-[20px] text-black'>각 분야 전문가의 프로필을 확인하고 1:1 맞춤 상담을 받아 보세요</p>
             </div>
 
             {/* 세 번째 카드 */}
-            <div className='relative h-[260px] w-[240px] rounded-lg border border-gray-300 bg-white p-4 shadow transition-all duration-300 hover:border-[#AEAC9A] hover:shadow-lg'>
+            <div className='relative h-[260px] w-full rounded-lg border border-gray-300 bg-white p-4 transition-all duration-300 hover:border-[#AEAC9A] hover:shadow-lg'>
               <img src='ai-image3.png' alt='Community' className='absolute left-[40px] h-[160px] w-[180px]' />
               <h3 className='absolute left-[15px] top-[160px] text-base font-bold leading-[25px] text-black'>Community</h3>
               <p className='absolute left-[15px] top-[190px] w-[210px] text-sm font-normal leading-[20px] text-black'>게시판과 채팅방을 통해 누떡 유저들과의 커뮤니티를 형성하세요</p>
             </div>
 
             {/* 네 번째 카드 */}
-            <div className='relative h-[260px] w-[240px] rounded-lg border border-gray-300 bg-white p-4 shadow transition-all duration-300 hover:border-[#AEAC9A] hover:shadow-lg'>
+            <div className='relative h-[260px] w-full rounded-lg border border-gray-300 bg-white p-4 transition-all duration-300 hover:border-[#AEAC9A] hover:shadow-lg'>
               <img src='ai-image4.png' alt='Life Graph' className='absolute left-[50px] top-[20px] h-[140px] w-[130px]' />
               <h3 className='absolute left-[15px] top-[160px] text-base font-bold leading-[25px] text-black'>Life Graph</h3>
               <p className='absolute left-[15px] top-[190px] w-[210px] text-sm font-normal leading-[20px] text-black'>인생의 여러 가지 사건들을 그래프로 만들어 보세요</p>
@@ -261,26 +258,36 @@ const HomePage = () => {
           <h2 className='mb-4 text-2xl font-bold'>지금 뜨는 게시글</h2>
           <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3'>
             {posts.length > 0 ? (
-              posts.map((post) => {
+              posts.map((post, index) => {
                 // postId 값이 유효한지 확인
                 if (!post.postId || isNaN(post.postId)) {
                   console.warn('Invalid postId:', post.postId);
                   return null; // 유효하지 않은 postId인 경우 렌더링하지 않음
                 }
 
-                // 날짜 값 검증
+                // 현재 날짜와 게시글의 작성 날짜를 비교
                 const postDate = new Date(post.createdAt);
+                const currentDate = new Date();
+                const isToday = postDate.toDateString() === currentDate.toDateString();
                 const isValidDate = !isNaN(postDate.getTime());
+
+                // 조회수가 가장 높은 게시글 확인
+                const isHighestView = index === 0;
 
                 return (
                   <div
                     key={post.postId}
-                    className='flex h-[220px] cursor-pointer flex-col justify-between rounded-lg border border-gray-300 bg-white p-4 transition-all duration-300 hover:border-[#AEAC9A] hover:shadow-lg'
+                    className='flex h-[160px] cursor-pointer flex-col justify-between rounded-lg border border-gray-300 bg-white p-4 transition-all duration-300 hover:border-[#AEAC9A] hover:shadow-lg'
                     onClick={() => navigate(`/community/${post.postId}`)}
                   >
                     {/* Post Title */}
-                    <div>
-                      <h3 className='mb-2 text-lg font-bold text-black'>{post.title}</h3>
+                    <div className='flex items-center'>
+                      <h3 className='mb-2 text-lg font-bold text-black'>
+                        {post.title}
+                        {/* NEW 또는 HOT 레이블을 제목 끝에 배치 */}
+                        {isToday && <span className="ml-2 inline-block align-middle font-['Pretendard'] text-[14px] font-medium leading-tight text-[#A1DFFF]">NEW!</span>}
+                        {isHighestView && <span className="ml-2 inline-block align-middle font-['Pretendard'] text-[14px] font-medium leading-tight text-[#FFC5C3]">HOT!</span>}
+                      </h3>
                     </div>
 
                     {/* Author Info */}
