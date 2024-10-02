6ca49199629a6d500a169c83a35d2e5bf806d0d3
[AI11] 김민지 <kkimming__@naver.com>
2024-10-02 19:11:50 +0900

    🔥 Remove : dotenv

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,6 @@
 import react from '@vitejs/plugin-react';
-import { config } from 'dotenv'; // dotenv 임포트 추가
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-
-// dotenv 설정
-config(); // .env 파일의 환경 변수를 로드합니다.
 
 export default defineConfig(({ mode }) => {
   // 환경 변수 로드
